193d79e0b0564463eb6eafb666ce1cb5614f85d3
Levi Vandenbempt <levi.vandenbempt.sjcm@gmail.com>
2019-12-01 16:59:07 +0100

    Update solve.js
    Show answers for both exercices

--- day01/solve.js
+++ day01/solve.js
@@ -1,19 +1,24 @@
 const fs = require('fs')
 
 const input = fs.readFileSync('input.txt', 'utf8');
-const moduleWeights = input.split('\n').filter(w => !!w);
+const moduleWeights = input.split('\n').filter(w => !!w).map(w => parseInt(w.trim()));
 
 function getFuelRequirement(weight) {
-        const fuel = Math.floor(weight / 3) - 2;
+        return Math.floor(weight / 3) - 2;
+}
+
+function getFuelRequirementRecursive(weight) {
+        const fuel = getFuelRequirement(weight);
         if (fuel > 0) {
-                return getFuelRequirement(fuel) + fuel;
+                return getFuelRequirementRecursive(fuel) + fuel;
         } else {
                 return 0;
         }
 }
 
-const fuelPerModule = moduleWeights.map(getFuelRequirement)
-const totalFuel = fuelPerModule.reduce((f1, f2) => f1 + f2)
+const fuelPerModuleWithoutRecursion = moduleWeights.map(getFuelRequirement);
+const fuelPerModule = moduleWeights.map(getFuelRequirementRecursive);
+const totalFuel = fuelPerModule.reduce((f1, f2) => f1 + f2);
 
-console.log("Total module weight: " + moduleWeights.reduce((w1, w2) => w1 + w2));
+console.log("Fuel requirement without recursion: " + fuelPerModuleWithoutRecursion.reduce((w1, w2) => w1 + w2));
 console.log("Total fuel requirement: " + totalFuel);
